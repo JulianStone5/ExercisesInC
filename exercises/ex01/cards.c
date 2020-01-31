15f527a9637d26c5dd79b73a397152bbee5f6fbe
Julian Stone <julian.stone@students.olin.edu>
2020-01-30 19:37:03 -0500

    Finished exercise 1

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,35 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Converts a card label to a number value assuming it is a legal card.
+   Otherwise, assigns value to 0.
+
+   card: string representing the card label
+*/
+int valAssign(char * card) {
+  int val = 0;
+  switch(card[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+      val = 10;
+      break;
+    case 'A':
+      val = 11;
+      break;
+    default:
+      val = atoi(card);
+  }
+  return val;
+}
+
+/* Takes a card value and returns the amount it will change the count.
+
+   val: value of the card that is to change the count value
+*/
+int changeCount(int val) {
+  if((val > 2) && (val < 7)) {
+    return 1;
+  } else if(val == 10) {
+    return -1;
+  }
+  return 0;
+}
+
+/* Executes the card counting algorithm as long as the user wants. If the
+   user inputs an "illegal" card, they will be notified. Otherwise, the
+   count value will change until the user inputs "X".
+*/
 int main() {
-  char card_name[3];
   int count = 0;
+  int val, change;
+  char card_name[3];
   while(card_name[0] != 'X') {
     puts("Enter the card name: ");
     scanf("%2s", card_name);
-    int val = 0;
-    switch(card_name[0]) {
-      case 'K':
-      case 'Q':
-      case 'J':
-        val = 10;
-        break;
-      case 'A':
-        val = 11;
-        break;
-      case 'X':
-        continue;
-      default:
-        val = atoi(card_name);
-        if((val < 1) || (val > 10)) {
-          puts("I don't understand that value!");
-          continue;
-        }
+    if(card_name[0] == 'X') {continue;}
+    val = valAssign(card_name);
+    change = changeCount(val);
+    if((val < 1) || (val > 10)) {
+      puts("I don't understand that value!");
+      continue;
     }
-    if((val > 2) && (val < 7)) {
-      count++;
-    } else if(val == 10) {
-      count--;
-    }
+    count = count + change;
     printf("Current count: %i\n", count);
   }
   return 0;
--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+/* Lines 17-18 seem to represent line 6 here
+
+   After optimization, there are less lines in the assemply code
+
+   After changing the print statement, the assembly code was once again
+   shorter with optimization
+
+   The optimized version seems to shorten the code by removing as much
+   referencing to the "5" within x. There doesn't seem to be any reference
+   to that "5", meaning less space is going to be used, which is good.
+*/
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  int x = 5;
+  int y = x + 1;
+  printf("%d",y);
+  return 0;
 }
