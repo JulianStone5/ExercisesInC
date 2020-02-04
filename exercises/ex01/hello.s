635e25eab649b46eba37261208708482ab2d09f2
Julian Stone <julian.stone@students.olin.edu>
2020-02-04 13:57:24 -0500

    Inclass optimization testing

--- exercises/ex01/hello.s
+++ exercises/ex01/hello.s
@@ -1,28 +1,61 @@
 	.file	"hello.c"
 	.text
-	.section	.rodata.str1.1,"aMS",@progbits,1
+	.section	.rodata
 .LC0:
-	.string	"%d"
-	.section	.text.startup,"ax",@progbits
-	.p2align 4,,15
+	.string	"c is %d\n"
+.LC1:
+	.string	"c is even"
+.LC2:
+	.string	"c is odd"
+	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB23:
+.LFB0:
 	.cfi_startproc
-	leaq	.LC0(%rip), %rsi
-	subq	$8, %rsp
+	pushq	%rbp
 	.cfi_def_cfa_offset 16
-	movl	$6, %edx
-	movl	$1, %edi
-	xorl	%eax, %eax
-	call	__printf_chk@PLT
-	xorl	%eax, %eax
-	addq	$8, %rsp
-	.cfi_def_cfa_offset 8
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$3, -12(%rbp)
+	movl	$4, -8(%rbp)
+	movl	-12(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	-4(%rbp), %eax
+	cltd
+	shrl	$31, %edx
+	addl	%edx, %eax
+	andl	$1, %eax
+	subl	%edx, %eax
+	testl	%eax, %eax
+	je	.L3
+	cmpl	$1, %eax
+	je	.L4
+	jmp	.L2
+.L3:
+	leaq	.LC1(%rip), %rdi
+	call	puts@PLT
+	jmp	.L2
+.L4:
+	leaq	.LC2(%rip), %rdi
+	call	puts@PLT
+	nop
+.L2:
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE23:
+.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
 	.section	.note.GNU-stack,"",@progbits
