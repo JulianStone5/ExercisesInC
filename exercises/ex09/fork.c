85d855e0b9a30d5e43a2d3654f592bb5104a3a11
Julian Stone <julian.stone@students.olin.edu>
2020-04-13 21:58:41 -0500

    Finished HW 9

--- exercises/ex09/fork.c
+++ exercises/ex09/fork.c
@@ -5,6 +5,22 @@ License: MIT License https://opensource.org/licenses/MIT
 
 */
 
+/*
+After adding some code, it seems that the child and parent
+don't share globals for sure despite the address being the
+same. As for the stack, they don't seem to share as the
+child will have it's own pointers. Lastly, I can't seem to
+figure out how to definitively say if they share a heap. I
+know they should, but whenever allocating new variables,
+they all have the same address rather than the heap growing.
+If I'm correct, while they point to the same addresses,
+they are actually different physical addresses, so they are
+correctly sharing the heap.
+
+I couldn't figure out how to check static components, but
+I believe they are not shared.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,7 +35,7 @@ License: MIT License https://opensource.org/licenses/MIT
 // error information
 extern int errno;
 
-
+int var1 = 0;
 // get_seconds returns the number of seconds since the
 // beginning of the day, with microsecond precision
 double get_seconds() {
@@ -32,8 +48,13 @@ double get_seconds() {
 
 void child_code(int i)
 {
-    sleep(i);
     printf("Hello from child %d.\n", i);
+    int var2 = 1;
+    sleep(i);
+    printf("Child %d's var1 pointer: %p\n", i, &var1);
+    printf("Child %d's var2 pointer: %p\n", i, &var2);
+    void *var3 = malloc(128);
+    printf("Child %d's var3 pointer: %p\n", i, var3);
 }
 
 // main takes two parameters: argc is the number of command-line
@@ -79,6 +100,8 @@ int main(int argc, char *argv[])
 
     /* parent continues */
     printf("Hello from the parent.\n");
+    void *var4 = malloc(128);
+    printf("Parent's var4 pointer: %p\n", var4);
 
     for (i=0; i<num_children; i++) {
         pid = wait(&status);
